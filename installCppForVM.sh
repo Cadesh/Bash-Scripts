5598f63c7e1eca4a5d560afa8ffde3805049b8ff
Andre <andrewvrosa@gmail.com>
2020-04-08 16:08:50 -0700

    Add files via upload

---
+++ installCppForVM.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# setup ubuntu vm for c programming
+# author: Andre Rosa
+# date: 03FEB2020
+
+apt update
+apt install gcc -y
+apt install g++ -y
+apt install vim
+apt install make
+apt install net-tools
+apt install openssh-server -y
