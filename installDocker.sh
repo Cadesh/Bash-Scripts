45f5510b8bb68bc070a46655b0f0fc5e7fc5b07f
Andre <andrewvrosa@gmail.com>
2020-04-10 08:16:56 -0700

    Docker for Ubuntu 18.04

--- installDocker.sh
+++ installDocker.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 # install docker on Ubuntu 18.04
-
-sudo apt update
-sudo apt update -q
+# to claan windows /r errors use:  sed -i 's/\r//' <scriptname>
+apt update
+apt update -q
 
 # install prerequisites
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 # add key for docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # add the repository to apt
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-sudo apt update
+apt update
 
 apt-cache policy docker-ce
-sudo apt install docker-ce -y
+apt install docker-ce -y
 # check if it is running
-sudo systemctl status docker
-
-# install docker-compose
-apt install docker-compose -y
+systemctl status docker
 
 # To later add an user to run without sudo
 #sudo usermod -aG docker ${USER}
